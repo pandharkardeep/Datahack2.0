42c63321fbdaec7c22f8eda20440b8b7c1c4392f
Deep P <pandharkardeep35@gmail.com>
2023-10-30 12:25:20 +0530

    DATAHACK

--- src/components/Streamlit.jsx
+++ src/components/Streamlit.jsx
@@ -5,7 +5,7 @@ const Streamlit = () => {
     <div className=' text-white mt-20'>
       {/* hello
       <a className='text-white px-32' href="https://diet-recommendation-system.streamlit.app/Diet_Recommendation" target='_blank'>Model</a> */}
-      <iframe className='h-[100vh] w-[200vh]' src="http://localhost:8502/" frameborder="0"></iframe>
+      <iframe className='h-[100vh] w-[200vh]' src="http://localhost:8501/" frameborder="0"></iframe>
     </div>
   )
 }
